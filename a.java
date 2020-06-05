2666d54b20ee935dae4a864bc7e72d438101cd57
sintu mukherjee <66489031+Sintu13@users.noreply.github.com>
2020-06-06 00:42:10 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,5 +1,5 @@
 public class a {
   public static void main(String[] args){
-    System.out.println("This is my first program in java");
+    System.out.println("This is my first program in java from git");
   }//End of main
 }//End of FirstJavaProgram Class
